f881cdb4652d015f2ed731f7056fda21943f799d
沉鳞竞跃 <13889278385@126.com>
2026-08-03 09:30:53 +0800

    dict --> .get（键，兜底值）的使用

--- dict.py
+++ dict.py
@@ -1,8 +1,10 @@
 user = {"name": "小李", "age": 20}
 user["city"] = "杭州"
 #新增键city，字典变为：{'name': '小李', 'age': 20, 'city': '杭州'}
-email = user.get("email", "未填写") 
+email = user.get("email", "未填写") #   .get(键, 兜底值)：找不到键不报错，给默认值。
 #标准函数  user.get(键, 默认值)：找字典里有没有 email 这个键
 #标准格式  字典.get(要查找的键, 找不到时返回的默认值)
 for key, value in user.items():
     print(key, value)   
+
+print(email)
